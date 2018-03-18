9aa4d165346579d1b212272219e952d958acacb0
SHOTSUN <k-vlad-k@mail.ru>
2018-03-18 20:51:48 +0300

    animation to blocks

--- dscan/sites/all/themes/bootstrap_dscan/js/script.js
+++ dscan/sites/all/themes/bootstrap_dscan/js/script.js
@@ -11,3 +11,27 @@ jQuery(document).ready(function() {
     offset: 100
   });
 });
+
+
+
+jQuery(document).ready(function() {
+  jQuery('.carousel-caption').addClass("hhh").viewportChecker({
+    classToAdd: 'vvv animated fadeIn',
+    offset: 100
+  });
+});
+
+
+jQuery(document).ready(function() {
+  jQuery('.services-reasons-item-num').addClass("hhh").viewportChecker({
+    classToAdd: 'vvv animated rotateIn',
+    offset: 100
+  });
+});
+
+jQuery(document).ready(function() {
+  jQuery('.to_up').addClass("hhh").viewportChecker({
+    classToAdd: 'vvv animated fadeInUp',
+    offset: 30
+  });
+});
